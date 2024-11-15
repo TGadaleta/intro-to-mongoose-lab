fbf4ee1a0e45a50e0c3002de3e7e975ca0258c67
Tony Gadaleta <anthonygadaleta@gmail.com>
2024-11-15 10:22:49 -0500

    view all customers working

--- app.js
+++ app.js
@@ -5,24 +5,42 @@ const prompt = require("prompt-sync")();
 const Customer = require("./models/customer");
 
 const connect = async () => {
-  await mongoose.connect(process.env.LOCAL_MONGODB_URI);
+  await mongoose.connect(process.env.MONGODB_URI);
   process.stdout.write('\x1Bc') //ChatGPT
-  console.log("Welcome to the MCR CRM!\n");
-  chooseMethod()
+  console.log("Welcome to the MCR CRM!");
+  chooseMethod();
 };
 
 const chooseMethod = async () => {
-  console.log("What would you like to do?\n");
+  console.log("\nWhat would you like to do?\n");
   console.log("  1. Create a customer");
   console.log("  2. View all customers");
   console.log("  3. Update a customer");
   console.log("  4. Delete a customer");
   console.log("  5. Quit\n");
   let methodNum = prompt("Number of action to run: ");
-  console.log(methodNum);
+  
+  switch (methodNum) {
+    case '1':
+        createCustomer();
+        break;
+    case '2':
+        viewAllCustomers();
+        break
+    case '5':
+        await mongoose.disconnect();
+        console.log('\nTo the end...');
+        process.exit();
+    default:
+        process.stdout.write('\x1Bc')
+        console.log("\nI'm afraid I can't do that. Choose again.")
+        chooseMethod();
+        break;
+  }
 };
 
 const createCustomer = async () => {
+    process.stdout.write('\x1Bc')
     const customerName = prompt("Enter the new customers name: ");
     const customerAge = prompt("Enter the customer's age: ");
 
@@ -32,7 +50,19 @@ const createCustomer = async () => {
     }
 
     const customer = await Customer.create(customerRecord)
-    console.log("New Customer: ", customer)
+    console.log("\nNew Customer:", customer.name, customer.age)
+    chooseMethod();
+}
+
+const viewAllCustomers = async () => {
+    const allCustomers = await Customer.find({});
+    process.stdout.write('\x1Bc')
+    console.log('Here are all the customers in your database:\n')
+    allCustomers.forEach(customer => {
+        console.log(customer._id, `--`, `Name: ${customer.name}, Age: ${customer.age}`)
+    })
+    prompt("\nPress enter to continue.")
+    chooseMethod();
 }
 
 
