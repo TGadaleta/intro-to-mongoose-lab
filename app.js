b34cf6f4a7bfd322270b5f7aa57009a8f3b5eec8
Tony Gadaleta <anthonygadaleta@gmail.com>
2024-11-15 09:07:41 -0500

    createCustomer function works

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const prompt = require("prompt-sync")();
 const Customer = require("./models/customer");
 
 const connect = async () => {
-  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect(process.env.LOCAL_MONGODB_URI);
   process.stdout.write('\x1Bc') //ChatGPT
   console.log("Welcome to the MCR CRM!\n");
-  chooseMethod();
+  chooseMethod()
 };
 
 const chooseMethod = async () => {
@@ -18,11 +18,25 @@ const chooseMethod = async () => {
   console.log("  3. Update a customer");
   console.log("  4. Delete a customer");
   console.log("  5. Quit\n");
-
   let methodNum = prompt("Number of action to run: ");
   console.log(methodNum);
 };
 
+const createCustomer = async () => {
+    const customerName = prompt("Enter the new customers name: ");
+    const customerAge = prompt("Enter the customer's age: ");
+
+    const customerRecord = {
+        name: customerName,
+        age: customerAge
+    }
+
+    const customer = await Customer.create(customerRecord)
+    console.log("New Customer: ", customer)
+}
+
+
+//connect();
 connect();
 
 // const username = prompt('What is your name? ');
